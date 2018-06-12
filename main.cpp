1f8e649bbc921fc39842b078138bbdf5faa5df10
Martin Hedberg <96mah002@gmail.com>
2018-06-12 22:05:18 +0200

    Added swapchain image view

--- main.cpp
+++ main.cpp
@@ -121,6 +121,9 @@ private:
 	VkFormat swapChainImageFormat;
 	VkExtent2D swapChainExtent;
 
+	//Image views to view the frames
+	vector<VkImageView> swapChainImageViews;
+
 	void initWindow() {
 		//Init GLFW lib.
 		glfwInit();
@@ -142,6 +145,8 @@ private:
 		pickPhysicalDevice();
 		createLogicalDevice();
 		createSwapChain();
+		createImageViews();
+		createGraphicsPipeline();
 	}
 
 	void createInstance() {
@@ -334,6 +339,33 @@ private:
 
 		swapChainImageFormat = surfaceFormat.format;
 		swapChainExtent = extent;
+	}
+
+	void createImageViews() {
+		swapChainImageViews.resize(swapChainImages.size());
+
+		for (size_t i = 0; i < swapChainImages.size(); i++) {
+			VkImageViewCreateInfo createInfo = {};
+			createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+			createInfo.image = swapChainImages[i];
+			createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+			createInfo.format = swapChainImageFormat;
+
+			createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+			createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			createInfo.subresourceRange.baseMipLevel = 0;
+			createInfo.subresourceRange.levelCount = 1;
+			createInfo.subresourceRange.baseArrayLayer = 0;
+			createInfo.subresourceRange.layerCount = 1;
+
+			if (vkCreateImageView(logicDevice, &createInfo, nullptr, &swapChainImageViews[i]) != VK_SUCCESS) {
+				throw runtime_error("Failed to create image views!");
+			}
+		}
 	}
 
 	QueueFamilyIndices findQueueFamily(VkPhysicalDevice device) {
@@ -535,6 +567,9 @@ private:
 	}
 
 	void cleanup() {
+		for (VkImageView imageView : swapChainImageViews) {
+			vkDestroyImageView(logicDevice, imageView, nullptr);
+		}
 		vkDestroySwapchainKHR(logicDevice, swapChain, nullptr);
 		vkDestroyDevice(logicDevice, nullptr);
 		if (enableValidationLayers) {
