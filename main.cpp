df7944adb3ba5e6d19775ca12e9684cb3b0a921d
Martin Hedberg <96mah002@gmail.com>
2018-12-12 22:10:56 +0100

    Implemented staging buffer

--- main.cpp
+++ main.cpp
@@ -700,36 +700,85 @@ private:
 	}
 
 	void createVertexBuffer() {
+		VkDeviceSize bufferSize = sizeof(vertices[0]) * vertices.size();
+
+		VkBuffer stagingBuffer;
+		VkDeviceMemory stagingBufferMemory;
+		createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
+
+		void* data;
+		vkMapMemory(logicDevice, stagingBufferMemory, 0, bufferSize, 0, &data);
+		//To remove, slow
+		memcpy(data, vertices.data(), (size_t)bufferSize);
+		vkUnmapMemory(logicDevice, stagingBufferMemory);
+
+		createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, vertexBuffer, vertexBufferMemory);
+		copyBuffer(stagingBuffer, vertexBuffer, bufferSize);
+
+		vkDestroyBuffer(logicDevice, stagingBuffer, nullptr);
+		vkFreeMemory(logicDevice, stagingBufferMemory, nullptr);
+	}
+
+	//TODO Use independent commandpool for meme transferes. Use VK_COMMAND_POOL_CREATE_TRANSIENT_BIT.
+	void copyBuffer(VkBuffer srcBuffer, VkBuffer destBuffer, VkDeviceSize size) {
+		VkCommandBufferAllocateInfo allocInfo = {};
+		allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+		allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+		allocInfo.commandPool = commandPool;
+		allocInfo.commandBufferCount = 1;
+
+		VkCommandBuffer commandBuffer;
+		vkAllocateCommandBuffers(logicDevice, &allocInfo, &commandBuffer);
+
+		VkCommandBufferBeginInfo beginInfo = {};
+		beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+		beginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+
+		vkBeginCommandBuffer(commandBuffer, &beginInfo);
+
+		VkBufferCopy copyRegion = {};
+		copyRegion.srcOffset = 0; //Optional
+		copyRegion.dstOffset = 0; //Optional
+		copyRegion.size = size;
+		vkCmdCopyBuffer(commandBuffer, srcBuffer, destBuffer, 1, &copyRegion);
+
+		vkEndCommandBuffer(commandBuffer);
+
+		VkSubmitInfo submitInfo = {};
+		submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+		submitInfo.commandBufferCount = 1;
+		submitInfo.pCommandBuffers = &commandBuffer;
+
+		vkQueueSubmit(graphicsQueue, 1, &submitInfo, VK_NULL_HANDLE);
+		vkQueueWaitIdle(graphicsQueue);
+
+		vkFreeCommandBuffers(logicDevice, commandPool, 1, &commandBuffer);
+	}
+
+	void createBuffer(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer &buffer, VkDeviceMemory &bufferMemory) {
 		VkBufferCreateInfo bufferInfo = {};
 		bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
-		bufferInfo.size = sizeof(vertices[0]) * vertices.size();
-		bufferInfo.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
+		bufferInfo.size = size;
+		bufferInfo.usage = usage;
 		bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
 
-		if (vkCreateBuffer(logicDevice, &bufferInfo, nullptr, &vertexBuffer) != VK_SUCCESS) {
+		if (vkCreateBuffer(logicDevice, &bufferInfo, nullptr, &buffer) != VK_SUCCESS) {
 			throw runtime_error("Failed to create verteb buffer!");
 		}
 
 		VkMemoryRequirements memReqs;
-		vkGetBufferMemoryRequirements(logicDevice, vertexBuffer, &memReqs);
+		vkGetBufferMemoryRequirements(logicDevice, buffer, &memReqs);
 
 		VkMemoryAllocateInfo allocInfo = {};
 		allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
 		allocInfo.allocationSize = memReqs.size;
-		allocInfo.memoryTypeIndex = findMemoryType(memReqs.memoryTypeBits, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+		allocInfo.memoryTypeIndex = findMemoryType(memReqs.memoryTypeBits, properties);
 
-		if (vkAllocateMemory(logicDevice, &allocInfo, nullptr, &vertexBufferMemory) != VK_SUCCESS) {
+		if (vkAllocateMemory(logicDevice, &allocInfo, nullptr, &bufferMemory) != VK_SUCCESS) {
 			throw runtime_error("Failed to allocate memory to the vertex buffer!");
 		}
 
-		vkBindBufferMemory(logicDevice, vertexBuffer, vertexBufferMemory, 0);
-
-		void* data;
-		vkMapMemory(logicDevice, vertexBufferMemory, 0, bufferInfo.size, 0, &data);
-
-		//To remove, slow
-		memcpy(data, vertices.data(), (size_t)bufferInfo.size);
-		vkUnmapMemory(logicDevice, vertexBufferMemory);
+		vkBindBufferMemory(logicDevice, buffer, bufferMemory, 0);
 	}
 
 	uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties) {
@@ -1146,7 +1195,7 @@ private:
 
 int main() {
 
-	//Next, stagingbuffer
+	//Next, index buffer
 	Application app;
 
 	try {
@@ -1159,3 +1208,5 @@ int main() {
 
 	return EXIT_SUCCESS;
 }
+
+//TODO Modify to use diffrent queue families for buffercopy, chapter stagingbuffers
